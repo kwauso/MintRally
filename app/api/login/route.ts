fd19ebc88af9e827f63000fa2b88b56abd6109ef
kwauso <akiranieda@keio.jp>
2025-01-07 19:33:34 +0900

    could login

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -11,13 +11,13 @@ export async function POST(request: Request) {
     // ユーザーをデータベースから取得
     const user = await prisma.user.findUnique({ where: { name } });
     if (!user) {
-        return NextResponse.json({ error: 'ユーザーが見つかりません' }, { status: 400 });
+        return NextResponse.json({ error: 'ユーザーまたはパスワードが無効です' }, { status: 400 });
     }
 
     // パスワードの検証
     const isPasswordValid = await bcrypt.compare(pass, user.pass || '');
     if (!isPasswordValid) {
-        return NextResponse.json({ error: 'パスワードが無効です' }, { status: 400 });
+        return NextResponse.json({ error: 'ユーザーまたはパスワードが無効です' }, { status: 400 });
     }
 
     // JWTトークンの生成
