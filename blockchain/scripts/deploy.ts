711437d7ccca2b78dbfb18009e0e926e730f55b3
kwauso <akiranieda@keio.jp>
2025-01-22 00:20:54 +0900

    I dont know how but it worked

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -8,11 +8,6 @@ async function main() {
   const address = await eventNFT.getAddress();
   console.log("EventNFT deployed to:", address);
 
-  const fs = require("fs");
-  const envContent = `NEXT_PUBLIC_NFT_CONTRACT_ADDRESS=${address}`;
-  fs.writeFileSync('../.env.local', envContent);
-
-  console.log("Contract address saved to .env.local");
 }
 
 main().catch((error) => {
