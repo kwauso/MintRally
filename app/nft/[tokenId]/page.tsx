a0a15078231e11d8e1070e39a1e8e9f9b56adb05
kwauso <superkwauso@icloud.com>
2025-02-03 23:54:14 +0900

    fixed all paramas error

--- app/nft/[tokenId]/page.tsx
+++ app/nft/[tokenId]/page.tsx
@@ -1,5 +1,5 @@
 import NFTDetail from './NFTDetail'
 
-export default function Page({ params }: { params: Promise<{ tokenId: string }> }) {
-  return <NFTDetail tokenId={params.tokenId} />
+export default async function Page({ params }: { params: Promise<{ tokenId: string }> }) {
+  return <NFTDetail tokenId={(await params).tokenId} />
 } 
