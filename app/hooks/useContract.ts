07cccd09ccb43beb74d670300f73f2ab1747ec5f
kwauso <superkwauso@icloud.com>
2025-02-04 00:36:32 +0900

    fixed bigint

--- app/hooks/useContract.ts
+++ app/hooks/useContract.ts
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { BrowserProvider, Contract, Signer } from 'ethers'
 import { NFT_CONTRACT } from '../../app/config/contract'
+import { useState, useEffect } from 'react'
 
 export function useContract() {
-    const [contract, setContract] = useState<ethers.Contract | null>(null)
-    const [signer, setSigner] = useState<ethers.Signer | null>(null)
+    const [contract, setContract] = useState<Contract | null>(null)
+    const [signer, setSigner] = useState<Signer | null>(null)
 
     useEffect(() => {
         const initContract = async () => {
             if (typeof window.ethereum !== 'undefined') {
                 try {
-                    const provider = new ethers.providers.Web3Provider(window.ethereum)
+                    const provider = new BrowserProvider(window.ethereum)
                     await provider.send("eth_requestAccounts", [])
-                    const signer = provider.getSigner()
+                    const signer = await provider.getSigner()
                     setSigner(signer)
 
-                    const contract = new ethers.Contract(
+                    const contract = new Contract(
                         NFT_CONTRACT.address!,
                         NFT_CONTRACT.abi,
                         signer
