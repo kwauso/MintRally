56d7306632a48b724684fe46909f187b36115520
kwauso <akiranieda@keio.jp>
2025-02-09 14:54:59 +0900

    fixed next.config.json

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
     experimental: {
         serverActions: true,
     },
-    output: "standalone",
+    output: "standalone",  // サーバーレス環境で最適化
+    webpack(config) {
+        config.resolve.fallback = { fs: false };  // ファイルシステムの依存関係を無効化
+        return config;
+    },
 }
 
 module.exports = nextConfig;
