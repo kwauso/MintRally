b3093b547a2b9f2bee9b11f98b6d89a62f1f64f8
kwauso <akiranieda@keio.jp>
2025-02-09 17:52:59 +0900

    added CORS

--- next.config.js
+++ next.config.js
@@ -5,13 +5,33 @@ const nextConfig = {
     },
     output: "standalone",  // サーバーレス環境で最適化
     webpack(config) {
-        config.resolve.fallback = { fs: false };  // ファイルシステムの依存関係を無効化
+        config.resolve.fallback = { 
+            fs: false, 
+            'bls-eth-wasm': false,
+            path: false,
+            crypto: false
+        };
         config.experiments = {
             ...config.experiments,
             asyncWebAssembly: true,
+            topLevelAwait: true
         }
         return config;
     },
+    async headers() {
+        return [
+            {
+                // すべてのルートに対してCORS設定を適用
+                source: '/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+                    { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig;
